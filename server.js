d825a9ca423618a961febf80c6ada91d09738977
Daniel Oglesby <dgo4man1@gmail.com>
2024-01-06 14:08:40 -0600

    chore: update gitignore

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 import express from 'express';
 import { sql } from "@vercel/postgres";
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 const app = express();
+
+  try {
+    await psql`\copy pl_data FROM '/openpowerlifting.csv' WITH (FORMAT CSV, HEADER);`
+  }
+  catch(error){
+    console.log('Error importing csv:', error);
+  }
+
+
 
 // Define a simple API endpoint
 app.get('/api', async (req, res) => {
